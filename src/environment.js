549cc9c3fe54ede52961b6d9000677cb44a31e17
Aayushi-1010 <aayushit.ay99099@gmail.com>
2025-04-18 14:42:06 +0530

    added file

---
+++ src/environment.js
@@ -0,0 +1,3 @@
+export const API_URL = process.env.REACT_APP_API_URL;
+export const MAPS_JAVASCRIPT_API = "AIzaSyB3gbWPH39rCH2HD1G4OvyoxkjueEhGl8w";
+export const REFUND_API = "https://stripe-server-tau.vercel.app"
